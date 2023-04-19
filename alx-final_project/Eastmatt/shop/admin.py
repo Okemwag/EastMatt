b572c6165834200bd68a8327eaac42e45be361a9
Okemwag <gabrielokemwa83@gmail.com>
2023-04-19 12:07:17 +0300

    Initial Commit

---
+++ alx-final_project/Eastmatt/shop/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price','available', 'created', 'updated']
+    prepopulated_fields = {'slug': ('name',)}
+    list_editable = ['price', 'available']
+    list_filter = ['available', 'created', 'updated']
