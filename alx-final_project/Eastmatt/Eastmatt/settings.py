936ab7b6785dc515a0994e25ccdd9d10bd0e881f
Okemwag <gabrielokemwa83@gmail.com>
2023-04-19 13:46:03 +0300

    Added the Cart Feature

--- alx-final_project/Eastmatt/Eastmatt/settings.py
+++ alx-final_project/Eastmatt/Eastmatt/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
--- alx-final_project/Eastmatt/shop/admin.py
+++ alx-final_project/Eastmatt/shop/admin.py
@@ -3,7 +3,7 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
